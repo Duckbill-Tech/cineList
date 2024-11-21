31437c0d7fda75cb28acd76ee914c7d090b95077
katewitkoski <ka.lwitkoski@gmail.com>
2024-11-21 14:20:30 -0300

    resolve teste da aplicação com as variaveis de ambiente

--- src/test/java/com/duckbill/cine_list/CineListApplicationTests.java
+++ src/test/java/com/duckbill/cine_list/CineListApplicationTests.java
@@ -1,11 +1,23 @@
 package com.duckbill.cine_list;
 
-import org.junit.jupiter.api.Disabled;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CineListApplicationTests {
+
+	@BeforeAll
+	static void setUp() {
+		// Carregar variáveis do .env no sistema
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> {
+			if (System.getProperty(entry.getKey()) == null) {
+				System.setProperty(entry.getKey(), entry.getValue());
+			}
+		});
+	}
 
 	@Test
 	void contextLoads() {
